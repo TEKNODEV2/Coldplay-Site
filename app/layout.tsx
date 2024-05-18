8c31e2e8e90994322caffb0ae6f3d494da16a341
Marco <tekno2.0developer@gmail.com>
2024-05-18 15:33:32 +0200

    dasdasd

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
 			<head />
 			<body
 				className={clsx(
-					"min-h-screen bg-background font-sans antialiased overflow-x-hidden",
+					"min-h-screen bg-background font-sans antialiased overflow-x-hidden ",
 					fontSans.variable
 				)}
 			>
