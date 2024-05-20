37b66e54db79e7fe4aff670a2578c3424668ce8c
Marco <tekno2.0developer@gmail.com>
2024-05-20 14:43:23 +0200

    sasdadasdasda

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -2,19 +2,21 @@
 import { useEffect, useState } from 'react';
 
 export default function ShopPage() {
-    const [hash, setHash] = useState(window.location.hash);
+    const [hash, setHash] = useState(typeof window !== 'undefined' ? window.location.hash : '');
 
     useEffect(() => {
-        const handleHashChange = () => {
-            setHash(window.location.hash);
-        };
+        if (typeof window !== 'undefined') {
+            const handleHashChange = () => {
+                setHash(window.location.hash);
+            };
 
-        window.addEventListener('hashchange', handleHashChange);
+            window.addEventListener('hashchange', handleHashChange);
 
-        // Pulizia dell'event listener quando il componente viene smontato
-        return () => {
-            window.removeEventListener('hashchange', handleHashChange);
-        };
+            // Pulizia dell'event listener quando il componente viene smontato
+            return () => {
+                window.removeEventListener('hashchange', handleHashChange);
+            };
+        }
     }, []);
 
     return (
